828036dd2b6ee292b48af7e3d7da314c627bd59f
Nafis Rahman <nafisrahman18@gmail.com>
2022-01-22 01:33:43 -0500

    set up application to build properly, edited OnlyApartmentsApplication.java and application.properties

--- OnlyApartments-Backend/src/main/java/ca/montreal/apartments/OnlyApartmentsApplication.java
+++ OnlyApartments-Backend/src/main/java/ca/montreal/apartments/OnlyApartmentsApplication.java
@@ -1,13 +1,21 @@
 package ca.montreal.apartments;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class OnlyApartmentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlyApartmentsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OnlyApartmentsApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
 }
