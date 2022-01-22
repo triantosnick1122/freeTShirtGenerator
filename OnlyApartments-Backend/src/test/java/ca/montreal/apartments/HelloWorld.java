33cabb7b6dc83a47518a0383c0a4912a9d49916c
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-01-22 01:08:46 -0500

    Add files via upload

---
+++ OnlyApartments-Backend/src/test/java/ca/montreal/apartments/HelloWorld.java
@@ -0,0 +1,14 @@
+package test;
+
+public class HelloWorld
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Nafis Rahman, Gardner Hall Floor 5.");
+    }
+
+
+
+
+
+}
