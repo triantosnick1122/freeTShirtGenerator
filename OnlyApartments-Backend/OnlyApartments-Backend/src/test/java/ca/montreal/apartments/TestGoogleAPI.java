c26c0cc1d282de1bea00fa41173dd4ef97aacef1
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-01-22 01:07:34 -0500

    Add files via upload

---
+++ OnlyApartments-Backend/OnlyApartments-Backend/src/test/java/ca/montreal/apartments/TestGoogleAPI.java
@@ -0,0 +1,37 @@
+package test;
+import org.json.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class TestGoogleAPI {
+
+    // class to test basic functions of the Google Places API
+    private final String apiKey = "AIzaSyDpY2tpFV0aZD3gRWLJRl23dSLd3d9ENIo";
+    private final String outputType = "json";
+
+    public static void main (String [] args)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
