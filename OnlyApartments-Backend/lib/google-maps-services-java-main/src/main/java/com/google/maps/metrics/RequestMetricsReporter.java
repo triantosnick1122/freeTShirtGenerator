2e8e79425db8162d0001fd3449226f13dcecd7f4
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-01-22 19:03:23 -0500

    remove lib directory

--- OnlyApartments-Backend/lib/google-maps-services-java-main/src/main/java/com/google/maps/metrics/RequestMetricsReporter.java
+++
@@ -1,7 +0,0 @@
-package com.google.maps.metrics;
-
-/** A type to report common metrics shared among all request types. */
-public interface RequestMetricsReporter {
-
-  RequestMetrics newRequest(String requestName);
-}
